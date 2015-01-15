0892203d8924df08a313df0f8bb8fc7d08233550
Enrique Gonzalez <enrique.gonzalez@skookum.com>
2015-01-15 13:19:55 -0500

    Added browser wrapper to client.

--- lib/skoogle_docs/client.rb
+++ lib/skoogle_docs/client.rb
@@ -47,6 +47,27 @@ module SkoogleDocs
       yield(self) if block_given?
     end
 
+    # Returns a list of all documents accessible to this client
+    #
+    # @return [Array]
+    def documents
+      browser.documents
+    end
+
+    # Returns a document based on the provided ID
+    #
+    # @param doc_id [String] the id of the document to look for
+    #
+    # @raise [SkoogleDocs::Errors::DocumentNotFound] if no document was found
+    #
+    # @raise [SkoogleDocs::Errors::InvalidDocument] if the document is not a
+    #   Google Document
+    #
+    # @return [SkoogleDocs::Document]
+    def document_by_id(doc_id)
+      browser.document_by_id(doc_id)
+    end
+
     # Wraps the Google API credentials into a Hash
     #
     # @return [Hash]
@@ -107,6 +128,15 @@ module SkoogleDocs
       REDIRECT_URI
     end
 
+    # Wrapper for a SkoogleDocs::Browser instance
+    #
+    # @api private
+    #
+    # @return [SkoogleDocs::Browser]
+    def browser
+      @browser ||= SkoogleDocs::Browser.new(session)
+    end
+
     private
 
     # Wrapper for a SkoogleDocs::Session instance
@@ -116,15 +146,6 @@ module SkoogleDocs
     # @return [SkoogleDocs::Session]
     def session
       @session ||= SkoogleDocs::Session.new(self)
-    end
-
-    # Wrapper for a SkoogleDocs::Browser instance
-    #
-    # @api private
-    #
-    # @return [SkoogleDocs::Browser]
-    def browser
-      @browser ||= SkoogleDocs::Browser.new(session)
     end
   end
 end
--- spec/skoogle_docs/client_spec.rb
+++ spec/skoogle_docs/client_spec.rb
@@ -17,4 +17,24 @@ describe SkoogleDocs::Client do
       end
     end
   end
+
+  describe "#document_by_id" do
+    it "delegates the call to a SkoogleDocs::Browser instance" do
+      browser = double("SkoogleDocs::Browser")
+      allow(browser).to receive(:document_by_id)
+
+      expect(client).to receive(:browser).and_return(browser)
+      client.document_by_id("doc_id")
+    end
+  end
+
+  describe "#documents" do
+    it "delegates the call to a SkoogleDocs::Browser instance" do
+      browser = double("SkoogleDocs::Browser")
+      allow(browser).to receive(:documents)
+
+      expect(client).to receive(:browser).and_return(browser)
+      client.documents
+    end
+  end
 end
